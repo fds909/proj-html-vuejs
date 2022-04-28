b9431db9ca2caa77dd24f15194778c8463637723
Francesco De Sìmini <francescod995@gmail.com>
2022-04-28 13:13:30 +0200

    header + hero

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+  @import 'style/palette.scss';
+
+  // Reset
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Libre Bodoni', serif;
+  }
+
+  // Commons
+  .container {
+    width: 85%;
+    margin: 0 auto;
+  }
+
+  .btn {
+    text-decoration: none;
+    padding: 10px 30px;
+    text-align: center;
+    font-size: 14px;
+    font-weight: 500;
+    border-radius: 5px;
+  }
+
+  .btn-white {
+    background-color: white;
+    color: $daisy-bush;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,110 @@
+<template>
+  <header>
+    <div class="header-top">
+        <img class="logo" src="../assets/img/avada-bakery-logo.png" alt="Bakery Avada Logo">
+        <nav>
+            <ul>
+                <li v-for="(element, index) in headerMenu" :key="index">
+                    <a href="#">{{ element }}</a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa-solid fa-cart-shopping"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="hero">
+        <div class="container">
+            <div class="hero-content">
+                <div class="upper-text">fresh & tasty bakery every day</div>
+                <h1>The Perfect Fresh Bread</h1>
+                <p>Cras consequat lectus vestibulum tortor pulvinar, quis euismod nisl varius. Aliquam erat volutpat. Nullam quis saggittis nibh nulla is orci tempor.</p>
+                <a class="btn btn-white" href="#">Explore our products</a>
+            </div>
+        </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp',
+  data() {
+      return {
+          headerMenu: ['HOME', 'SHOP', 'ABOUT', 'GALLERY', 'LOCATIONS', 'JOURNAL', 'CONTACT', 'MY ACCOUNT']
+      }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+    @import '../style/palette.scss';
+
+    header {
+        font-family: 'Open Sans', sans-serif;
+        color: $daisy-bush;
+        background-image: url('../assets/img/hero-header-bakery.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 100vh;
+    }
+    
+    .logo {
+        height: 75px;
+    }
+
+    .header-top {
+        font-weight: 500;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 90px;
+        padding: 0 20px;
+    }
+
+    nav ul {
+        list-style-type: none;
+    }
+
+    nav li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    nav a {
+        text-decoration: none;
+        font-size: 14px;
+    }
+
+    // Hero
+    .hero {
+        padding: 100px 0;
+    }
+
+    .upper-text {
+        font-size: 12px;
+        font-weight: 500;
+        text-transform: uppercase;
+        color: $mamba;
+    }
+
+    .hero-content {
+        width: calc(100% / 3);
+    }
+
+    h1 {
+        font-family: 'Libre Bodoni', serif;
+        font-size: 45px;
+        font-weight: bold;
+        margin: 15px 0;
+    }
+
+    .hero p {
+        text-align: justify;
+        margin-bottom: 30px;
+        color: $mamba;
+        font-weight: 500;
+    }
+</style>
